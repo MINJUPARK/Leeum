1d6aaf2573d1cdfdad234b156e9781c009670588
MINJUPARK <psj0943@naver.com>
2022-11-28 01:35:37 +0900

    메인 작업중 (program 등)

--- sub/about/js/tab-menu.js
+++ sub/about/js/tab-menu.js
@@ -1,5 +1,6 @@
 $(function() {
     AOS.init();
+
     $('.tabmenu-wrap .tab-nav').find('a').on('click', function(e) {
         AOS.init();
         var $this = $(this);
@@ -13,4 +14,21 @@ $(function() {
         $tab_contents.hide();
         $(id).show();
     });
+
+    // 메인 '달라진 리움' 배너에서 -> 공용공간 리뉴얼 탭으로 이동
+    function specificTab(e) {
+        var hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+        }
+        if(hash[1] == 'renewal') {
+            $('.tabmenu-wrap .tab-nav').find('.nav').removeClass('on');
+            $('[href="#renewal"]').parent().addClass('on');
+            $('.tab-con').find('.con-box').hide();
+            $('.tab-con').find('#renewal').show();
+        }
+    }
+    specificTab();
 }) 
