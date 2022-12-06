abd2e75f20e7c29cf906da1a42611ac3b11fab7e
kimmido <icecream120102@gmail.com>
2022-12-06 16:04:39 +0900

    fancybox수정중

---
+++ sub/collect/js/conserve_fancybox.js
@@ -0,0 +1,6 @@
+// $(function() {
+//     function removeFancyBox() {
+//         $(".type-img a").on('click', e => e.preventDefault());
+//         $(".type-img a").removeAttr('data-fancybox');
+//     }
+// })
