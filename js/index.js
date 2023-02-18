6a206ec979d772d3292b5de920db78c5da68f1fb
MINJUPARK <psj0943@naver.com>
2023-02-19 03:46:39 +0900

    웹서버 업로드 이후 수정건 v3

--- js/index.js
+++ js/index.js
@@ -21,17 +21,3 @@ function setVisible(target, animate) {
         }
     }
 }
-
-$(function() {
-    $(window).on('scroll', function() {
-        setVisible($('section.exhibition'), 'fadeInUp');
-        setVisible($('section.collection .con'), 'fadeInUp animate__faster');
-        setVisible($('section.collection .antique'), 'fadeInLeft');
-        setVisible($('section.collection .modern'), 'fadeInRight');
-        setVisible($('section.facility .banner-box'), 'fadeInUp animate__faster');
-        setVisible($('section.facility .person'), 'fadeInLeft animate__delay-500ms');
-        setVisible($('section.facility .con'), 'fadeInRight animate__delay-1s');
-        setVisible($('section.program'), 'fadeInUp animate__faster');
-        setVisible($('section.artist'), 'fadeInUp');
-    })
-})
